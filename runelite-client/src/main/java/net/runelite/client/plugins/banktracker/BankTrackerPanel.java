155411b97a81f79ca54ba7dc5ecf8b23b8868a65
Henkka <henkkaah@windowslive.com>
2019-12-01 19:40:48 +0200

    BankTrackerPanel constructor

--- runelite-client/src/main/java/net/runelite/client/plugins/banktracker/BankTrackerPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/banktracker/BankTrackerPanel.java
@@ -3,14 +3,21 @@ package net.runelite.client.plugins.banktracker;
 import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.PluginPanel;
 
+import javax.inject.Inject;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
-// TODO
 public class BankTrackerPanel extends PluginPanel {
 
     JButton showGraphButton = new JButton("Show data");
     JButton clearUserData = new JButton("Clear user data");
+
+    private BankTrackerPlugin plugin;
+
+    @Inject
+    public BankTrackerPanel(final BankTrackerPlugin plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
     public void onActivate() {
